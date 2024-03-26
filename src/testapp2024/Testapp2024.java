278e85113e692763ec0fe98c6e73fe5fec595e88
samplex2 <resane01@gmail.com>
2024-03-26 14:20:14 +0800

    first commit

---
+++ src/testapp2024/Testapp2024.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testapp2024;
+
+/**
+ *
+ * @author SCC3
+ */
+public class Testapp2024 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
